4716f472757ad56e8be432d694a130a7dda2122d
yogeshgiteSQRepository <yogeshgite333@gmail.com>
2026-03-25 13:08:55 +0530

    Fixed region name

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "kopsstate7865"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-south-1"
   }
 
   required_version = "~> 1.6.3"
